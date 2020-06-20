0c1f8cef247644621bd582460b527a8372222422
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-06-20 15:46:56 +0200

    inline styling elements via object embedding css inline style applied to element via embedding css as an object

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ import ReactDOM from "react-dom";
 
 var name = "Jozef Drha";
 var year = new Date().getFullYear();
-const img = "https://picsum.photos/200"; //náhodné fotky dáva táto služba
+
+const obr = "https://picsum.photos/200"; //náhodné fotky dáva táto služba
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
 
 ReactDOM.render(
   <div>
-    <h1 className="heading">Hello World!</h1>
+    <h1 style={customStyle} contentEditable="true">
+      Hello World!
+    </h1>
     <p>This is the year {year}</p>
 
-    <img className="img-pasta" src={img} />
-    <img className="img-pasta" src={img} />
-    <img className="img-pasta" src={img} />
+    <img className="img-pasta" alt="random" src={obr} />
+    <img className="img-pasta" alt="random" src={obr} />
+    <img className="img-pasta" alt="random" src={obr} />
+    <img alt="random" src={obr} />
+    <button onClick={() => window.location.reload(false)}>
+      Click to reload!
+    </button>
   </div>,
   document.getElementById("root")
 );
