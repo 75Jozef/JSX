e24095699ad8220a898634dccd5ed59f1744083b
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-06-20 14:46:05 +0200

    JSX style applied script - language is defined in .html as JSX, not js; "className" is the right "class" to component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+var name = "Jozef Drha";
+var year = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">Hello World!</h1>
+    <p>This is the year {year}</p>
+
+    <ul>
+      <li>First</li>
+      <li>Second</li>
+      <li className="thirdOne">Third</li>
+      <li>Fourth</li>
+      <li>Fifth</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
